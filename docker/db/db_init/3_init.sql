0ee607d2486573d96ac48ecc67e729051b5e4fc9
imamiya-masaki <imachan567@gmail.com>
2020-10-01 04:13:26 +0900

    sql修正

--- docker/db/db_init/3_init.sql
+++ docker/db/db_init/3_init.sql
@@ -3,30 +3,30 @@ CREATE TABLE quetions (
     title VARCHAR(32), 
     content VARCHAR(256),
     created_at TIMESTAMP,
-    modifyed_at TIMESTAMP,
+    modifyed_at INT,
     group_id VARCHAR (256),
     privated BOOLEAN,
     create_user_id int
 );
 
-CREATE TABLE quetion_tags {
+CREATE TABLE quetion_tags (
     id int auto_increment not null primary key,
     target_id int,
     tag_id int
-}
+);
 
-CREATE TABLE tags {
+CREATE TABLE tags (
     id int auto_increment not null primary key,
     tag_name VARCHAR (256)
-}
+);
 
-CREATE TABLE user_ids {
+CREATE TABLE user_ids (
     id int auto_increment not null primary key,
     group_id int,
     name_info VARCHAR (256)
-}
+);
 
-CREATE TABLE group_ids {
+CREATE TABLE group_ids (
     id int auto_increment not null primary key,
     name_info  VARCHAR (256)
-}
+);
