c7119118e895f93bb8eefdb9dd511378108b92d2
imamiya-masaki <imachan567@gmail.com>
2020-12-13 06:52:32 +0900

    wip

--- app/database/database.go
+++ app/database/database.go
@@ -2,9 +2,9 @@ package database
 
 import (
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/appengine"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -19,14 +19,29 @@ func Init() {
 		log.Fatal("Error loading .env file")
 	}
 
-	DATABASE := os.Getenv("DATABASE")
-	USERNAME := os.Getenv("USERNAME")
-	USERPASS := os.Getenv("USERPASS")
-
+	// DATABASE := os.Getenv("DATABASE")
+	// USERNAME := os.Getenv("USERNAME")
+	// USERPASS := os.Getenv("USERPASS")
+	CONNECTIONNAME := "collecttq:asia-northeast1:collectq-mysql"
+	USER := "collectqUser"
+	PASS := "collectq"
+	DBNAME := "collectq_db"
+	// socketDir := "cloudsql"
+	// set := USER + ":" + PASS + "@cloudsql(" + cfg + ")/" + DBNAME
+	// dbURI := fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s", USER, PASS, cfg, DBNAME)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", USER, PASS, "127.0.0.1:3306", DBNAME)
 	// dsn := "docker:docker@tcp(mysql:3306)/collectq_db?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := USERNAME + ":" + USERPASS + "@tcp(mysql:3306)/" + DATABASE + "?charset=utf8mb4&parseTime=True&loc=Local"
-	println("dsn", dsn)
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := USERNAME + ":" + USERPASS + "@tcp(mysql:3306)/" + DATABASE + "?charset=utf8mb4&parseTime=True&loc=Local"
+	//println("dsn", dsn)
+	// db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	localConnection := USER + ":" + PASS + "@/" + DBNAME + "?parseTime=true"
+	cloudSQLConnection := USER + ":" + PASS + "@unix(/cloudsql/" + CONNECTIONNAME + ")/" + DBNAME + "?parseTime=true"
+	if appengine.IsAppEngine() {
+		db, err = gorm.Open(mysql.Open(cloudSQLConnection), &gorm.Config{})
+	} else {
+		db, err = gorm.Open(mysql.Open(localConnection), &gorm.Config{})
+	}
 	if err != nil {
 		println(err)
 	} else {
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql
+
+go 1.15
